1214e55b06450606a349a9635f223ef4efb4607e
golem <nohernan@gmail.com>
2019-08-01 18:38:23 -0500

    New version of estruc_rec.hs

--- sesion02/estruc_rec.hs
+++ sesion02/estruc_rec.hs
@@ -1,3 +1,26 @@
--- Definición de fórmulas de la lógica proposicional
-data FormProp = V | Atom String | No FormProp | Disy FormProp FormProp | Conj FormProp FormProp | Imp FormProp FormProp deriving Show
+data ValBool = V | F deriving (Show,Eq)
 
+-- Definición de fórmulas de la lógica proposicional
+data FormProp = Const ValBool | Atom Char | No FormProp | Disy FormProp FormProp | Conj FormProp FormProp | Imp FormProp FormProp deriving Show
+
+mi_asig :: Char -> ValBool
+mi_asig c
+  | c == 'p' = V
+  | c == 'q' = V
+  | otherwise = F
+
+inter :: (Char -> ValBool) -> FormProp -> ValBool
+inter _ (Const V) = V
+inter asig (Atom p) = asig p
+inter asig (No phi)
+  | (inter asig phi) == V = F
+  | otherwise = V
+inter asig (Disy phi psi) 
+  | (inter asig phi) == F && (inter asig psi) == F = F
+  | otherwise = V
+inter asig (Conj phi psi)
+  | (inter asig phi) == V && (inter asig psi) == V = V
+  | otherwise = F
+inter asig (Imp phi psi)
+  | (inter asig phi) == V && (inter asig psi) == F = F
+  | otherwise = V
